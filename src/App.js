8e952669ddc36072a0940b5941a65aa42b3b8782
pat123 <phollakrit@hotmail.com>
2022-09-30 11:50:42 +0700

    form-bootstrap.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import MessageBox from "./state-func";
+import FormBootstrap from './form-bootstrap';
 
 function App() {
-    return <MessageBox/>
+    return <FormBootstrap/>
 }
 export default App;
